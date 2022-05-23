0ff76e4dff61c66f953549b6fdb87f9a25c96bdb
Tóta Dávid <totadavid95@gmail.com>
2022-05-23 11:08:48 +0200

    Update auth.js
    - StatusCodes használata.
    - A secret key és az algorithm kiolvasása a .env fájlból.

--- graphql/routers/auth.js
+++ graphql/routers/auth.js
@@ -1,45 +1,48 @@
 const express = require("express");
 const router = express.Router();
-const models = require("../models");
+const { StatusCodes } = require("http-status-codes");
+const { User } = require("../models");
 const jwt = require("jsonwebtoken");
-const auth = require("../middlewares/auth");
-
-const { User } = models;
+const authMiddleware = require("../middlewares/auth");
 
 // localhost:4000/auth/login
-router.post("/login", async function (req, res) {
+router.post("/login", async (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) {
-        return res.status(400).send({ message: "Nem adtál meg emailt vagy jelszót!" });
+        return res.status(StatusCodes.BAD_REQUEST).send({ message: "Nem adtál meg emailt vagy jelszót!" });
     }
     const user = await User.findOne({ where: { email } });
     if (!user) {
-        return res.status(404).send({ message: "A megadott email címmel nem létezik felhasználó!" });
+        return res.status(StatusCodes.NOT_FOUND).send({ message: "A megadott email címmel nem létezik felhasználó!" });
     }
     if (user.comparePassword(password)) {
-        //return res.status(200).send({ message: "Sikeres bejelentkezés" });
-        const token = jwt.sign(user.toJSON(), "secret", { algorithm: "HS256" });
+        //return res.status(StatusCodes.OK).send({ message: "Sikeres bejelentkezés" });
+        const token = jwt.sign(user.toJSON(), process.env.JWT_SECRET || "secret", { algorithm: process.env.JWT_ALGO || "HS256" });
+        // Mivel létezik token nevű konstans, ezért a { token } egy rövidítés a { token: token } helyett:
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer
         return res.send({ token }); // = res.send({ token: token });
     }
-    return res.status(401).send({ message: "A megadott jelszó helytelen!" });
+    return res.status(StatusCodes.UNAUTHORIZED).send({ message: "A megadott jelszó helytelen!" });
 });
 
 // localhost:4000/auth/who
-router.get("/who", auth, async function (req, res) {
+// Ha érvényes a JWT token, akkor ez a végpont válaszban visszaadja a JWT token payload-ját (jwt.sign első paramétere)
+// A JWT token ellenőrizhető a https://jwt.io/ oldalon.
+router.get("/who", authMiddleware, async (req, res) => {
     //console.log(req.headers);
     res.send(req.user);
 });
 
 // localhost:4000/auth/register
-router.post("/register", async function (req, res) {
+router.post("/register", async (req, res) => {
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
-        return res.status(400).send({ message: "Nem adtál meg nevet, emailt vagy jelszót!" });
+        return res.status(StatusCodes.BAD_REQUEST).send({ message: "Nem adtál meg nevet, emailt vagy jelszót!" });
     }
     const user = await User.create({ name, email, password });
     // A regisztráció egyben egy bejelentkezés is, tehát egy tokent is aláírunk és elküldjük
-    const token = jwt.sign(user.toJSON(), "secret", { algorithm: "HS256" });
-    return res.status(201).send({ token });
+    const token = jwt.sign(user.toJSON(), process.env.JWT_SECRET || "secret", { algorithm: process.env.JWT_ALGO || "HS256" });
+    return res.status(StatusCodes.CREATED).send({ token });
 });
 
 module.exports = router;
